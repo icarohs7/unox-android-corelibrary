83265d0cab2d1c689e1ee107965ee9059b9d781b
Icaro Temponi <icarohs7@gmail.com>
2019-05-24 15:29:10 -0300

    auto commit push on 24/05/2019 15:29

--- src/main/java/base/corelibrary/presentation/adapters/DslAdaptersApi.kt
+++ src/main/java/base/corelibrary/presentation/adapters/DslAdaptersApi.kt
@@ -2,11 +2,18 @@ package base.corelibrary.presentation.adapters
 
 import android.widget.FrameLayout
 import androidx.annotation.MainThread
+import androidx.core.view.setMargins
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import base.corelibrary.R
 import base.corelibrary.databinding.ItemEmptyBinding
+import base.corelibrary.databinding.LayoutSwipeMenuEditDeleteBinding
 import base.corelibrary.databinding.LayoutSwipeMenuRightBinding
+import base.corelibrary.domain.extensions.plusAssign
+import kotlinx.coroutines.launch
+import splitties.dimensions.dip
+import splitties.systemservices.layoutInflater
 
 /**
  * Builder used to create a multi purpose adapter
@@ -76,6 +83,21 @@ fun <T> RecyclerView.renderRightSwipeContainerAdapter(
 ): BaseBindingAdapter<T, LayoutSwipeMenuRightBinding> {
     return useRightSwipeContainerAdapter {
         bind { item -> bindFun(item) }
+        if (items.isNotEmpty()) loadList(items)
+    }
+}
+
+fun <T> RecyclerView.renderSwipeEditDeleteMenu(
+        items: List<T> = emptyList(),
+        bindFun: (item: T, layoutContent: FrameLayout, swipeMenu: LayoutSwipeMenuEditDeleteBinding) -> Unit
+): BaseBindingAdapter<T, LayoutSwipeMenuRightBinding> {
+    return useRightSwipeContainerAdapter {
+        bind { item ->
+            val swipeMenu = LayoutSwipeMenuEditDeleteBinding.inflate(layoutInflater).apply {
+                layoutUnderContent += this
+            }
+            bindFun(item, layoutOverContent, swipeMenu)
+        }
         if (items.isNotEmpty()) loadList(items)
     }
 }
