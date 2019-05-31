22ce741ab0c7cf7a759dde6f1d280aacbe4adab7
Icaro Temponi <icarohs7@gmail.com>
2019-05-31 14:07:58 -0300

    auto commit push on 31/05/2019 14:07

--- src/main/java/base/corelibrary/domain/extensions/EditTextExtensions.kt
+++ src/main/java/base/corelibrary/domain/extensions/EditTextExtensions.kt
@@ -1,6 +1,19 @@
 package base.corelibrary.domain.extensions
 
+import android.text.InputType
 import android.widget.EditText
 
 val EditText.textOrEmpty: CharSequence
     get() = text?.toString().orEmpty()
+
+fun EditText.useNumberInputType() {
+    inputType = InputType.TYPE_CLASS_NUMBER
+}
+
+fun EditText.useDecimalNumberInputType() {
+    inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+}
+
+fun EditText.usePhoneInputType() {
+    inputType = InputType.TYPE_CLASS_PHONE
+}
