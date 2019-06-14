f85de6b27e992df087b917071e112b1996790890
Icaro Temponi <icarohs7@gmail.com>
2019-06-14 12:52:15 -0300

    auto commit push on 14/06/2019 12:52

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,18 @@ dependencies {
     AndroidKaptDeps.core.forEach(::kapt)
 }
 
+repositories {
+    google()
+    mavenLocal()
+    mavenCentral()
+    maven("http://devrepo.kakao.com:8088/nexus/content/groups/public/")
+    maven("https://kotlin.bintray.com/kotlinx")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://dl.bintray.com/icarohs7/libraries")
+    jcenter()
+    maven("https://jitpack.io")
+}
+
 setupJacoco {
     sourceDirectories.setFrom(files(
             android.sourceSets["main"].java.srcDirs,
