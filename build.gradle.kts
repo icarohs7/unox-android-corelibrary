09ebaa8b6af4aeec44cedfacb6f71e392ccc8586
Icaro Temponi <icarohs7@gmail.com>
2019-06-27 11:47:39 -0300

    auto commit push on 27/06/2019 11:47

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,3 +66,15 @@ setupJacoco {
             "src/main/kotlin"
     ))
 }
+
+repositories {
+    google()
+    mavenLocal()
+    mavenCentral()
+    maven("http://devrepo.kakao.com:8088/nexus/content/groups/public/")
+    maven("https://kotlin.bintray.com/kotlinx")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://dl.bintray.com/icarohs7/libraries")
+    jcenter()
+    maven("https://jitpack.io")
+}
