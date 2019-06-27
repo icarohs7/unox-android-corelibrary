43199af0bbeaf9047c6b31135fa551825171bc3e
Icaro Temponi <icarohs7@gmail.com>
2019-06-27 15:47:37 -0300

    auto commit push on 27/06/2019 15:47

--- src/main/java/base/corelibrary/presentation/fragments/BaseExtFabRecyclerFragment.kt
+++ src/main/java/base/corelibrary/presentation/fragments/BaseExtFabRecyclerFragment.kt
@@ -1,11 +1,23 @@
 package base.corelibrary.presentation.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import base.corelibrary.R
 import base.corelibrary.databinding.FragmentExtFabRecyclerBinding
 import base.corelibrary.databinding.FragmentSwipeRecyclerBinding
 import com.github.icarohs7.unoxandroidarch.presentation.fragments.BaseBindingFragment
 
 abstract class BaseExtFabRecyclerFragment : BaseBindingFragment<FragmentExtFabRecyclerBinding>() {
+    override fun onBindingCreated(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) {
+        super.onBindingCreated(inflater, container, savedInstanceState)
+        binding.stateView.hideStates()
+    }
+
+    fun displayState(stateTag: String) {
+        binding.stateView.displayState(stateTag)
+    }
+
     override fun getLayout(): Int {
         return R.layout.fragment_ext_fab_recycler
     }
