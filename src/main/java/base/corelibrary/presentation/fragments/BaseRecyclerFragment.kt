1074a06d0b95c9a0182b81d02b768e737020bbc8
Icaro Temponi <icarohs7@gmail.com>
2019-07-03 16:00:59 -0300

    auto commit push on 03/07/2019 16:00

---
+++ src/main/java/base/corelibrary/presentation/fragments/BaseRecyclerFragment.kt
@@ -0,0 +1,36 @@
+package base.corelibrary.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import base.corelibrary.R
+import base.corelibrary.databinding.FragmentBaseRecyclerBinding
+import com.github.icarohs7.unoxandroidarch.presentation.fragments.BaseBindingFragment
+import com.umutbey.stateviews.StateView
+
+/**
+ * Extension of [BaseBindingFragment] using a layout
+ * containing a fullscreen [StateView] with a [RecyclerView]
+ * inside
+ */
+abstract class BaseRecyclerFragment : BaseBindingFragment<FragmentBaseRecyclerBinding>() {
+    override fun onBindingCreated(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) {
+        super.onBindingCreated(inflater, container, savedInstanceState)
+        binding.stateView.hideStates()
+        binding.recycler.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    /**
+     * Display a state with the given tag
+     * on the state view of the layout
+     */
+    fun displayState(stateTag: String) {
+        binding.stateView.displayState(stateTag)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.fragment_base_recycler
+    }
+}
