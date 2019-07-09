47f6af2fea65f65151793c9dc36642ecd62a382c
Icaro Temponi <icarohs7@gmail.com>
2019-07-09 10:29:12 -0300

    auto commit push on 09/07/2019 10:29

--- src/main/java/base/corelibrary/domain/toplevel/Toasts.kt
+++ src/main/java/base/corelibrary/domain/toplevel/Toasts.kt
@@ -2,6 +2,7 @@ package base.corelibrary.domain.toplevel
 
 import android.content.Context
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -13,4 +14,12 @@ fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
 
 fun Context.toast(msgRes: Int, duration: Int = Toast.LENGTH_SHORT) {
     GlobalScope.launch(Dispatchers.Main) { Toast.makeText(this@toast, str(msgRes), duration).show() }
+}
+
+fun Fragment.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
+    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(requireContext(), msg, duration).show() }
+}
+
+fun Fragment.toast(msgRes: Int, duration: Int = Toast.LENGTH_SHORT) {
+    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(requireContext(), str(msgRes), duration).show() }
 }
