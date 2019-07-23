e4a7c7c4f63c8c0385f5e2802c93baa39834b321
Icaro Temponi <icarohs7@gmail.com>
2019-07-23 11:22:19 -0300

    auto commit push on 23/07/2019 11:22

--- src/main/java/base/corelibrary/domain/adapter/RoomTimeTypeConverters.kt
+++ src/main/java/base/corelibrary/domain/adapter/RoomTimeTypeConverters.kt
@@ -1,9 +1,6 @@
 package base.corelibrary.domain.adapter
 
 import androidx.room.TypeConverter
-import com.github.debop.kodatimes.milliDuration
-import org.joda.time.Duration
-import org.joda.time.Period
 import java.util.Date
 
 /**
@@ -19,25 +16,5 @@ class RoomTimeTypeConverters {
     @TypeConverter
     fun toDate(data: Long): Date {
         return Date(data)
-    }
-
-    @TypeConverter
-    fun fromPeriod(data: Period): Int {
-        return data.millis
-    }
-
-    @TypeConverter
-    fun toPeriod(data: Int): Period {
-        return Period.millis(data)
-    }
-
-    @TypeConverter
-    fun fromDuration(data: Duration): Long {
-        return data.millis
-    }
-
-    @TypeConverter
-    fun toDuration(data: Long): Duration {
-        return data.milliDuration()
     }
 }
