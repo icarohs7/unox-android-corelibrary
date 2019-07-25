bb75cbe2cf36ffa6b957d7217983b4bb4e8c111c
Icaro Temponi <icarohs7@gmail.com>
2019-07-25 14:30:29 -0300

    auto commit push on 25/07/2019 14:30

--- src/main/java/base/corelibrary/domain/toplevel/Coroutines.kt
+++ src/main/java/base/corelibrary/domain/toplevel/Coroutines.kt
@@ -3,6 +3,9 @@ package base.corelibrary.domain.toplevel
 import com.github.icarohs7.unoxandroidarch.toplevel.whileLoading
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
@@ -14,5 +17,15 @@ fun CoroutineScope.launchLoading(
 ) {
     launch(context, start) {
         whileLoading(fn = block)
+    }
+}
+
+/**
+ * Create a flow that emit once every [emissionDelayMillis]
+ */
+fun flowTimer(emissionDelayMillis: Long = 1000L): Flow<Unit> = flow {
+    while (true) {
+        emit(Unit)
+        delay(emissionDelayMillis)
     }
 }
