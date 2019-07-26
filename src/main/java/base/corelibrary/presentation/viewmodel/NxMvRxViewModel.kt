f0bcdc4e2b0d6b57ad65d8b8db641486476f9e5d
Icaro Temponi <icarohs7@gmail.com>
2019-07-26 08:36:08 -0300

    auto commit push on 26/07/2019 08:36

--- src/main/java/base/corelibrary/presentation/viewmodel/NxMvRxViewModel.kt
+++ src/main/java/base/corelibrary/presentation/viewmodel/NxMvRxViewModel.kt
@@ -10,7 +10,13 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.plus
 
 /**
  * Base ViewModel implementing a [CoroutineScope]
@@ -21,6 +27,26 @@ open class NxMvRxViewModel<S : MvRxState>(initialState: S) : BaseMvRxViewModel<S
         UnoxAndroidArch.isDebug,
         RealMvRxStateStore(initialState)
 ), CoroutineScope by MainScope() {
+    /**
+     * Connect the given [Flow] to the
+     * state of the viewmodel, updating
+     * the state on each emission
+     */
+    fun Flow<S>.connectToState(): Job {
+        return connectToState { this }
+    }
+
+    /**
+     * Use the given transformer and for each
+     * emission of the given [Flow], transform
+     * the emmited item and apply the reducer to
+     * evolve the state
+     */
+    fun <T> Flow<T>.connectToState(transformer: S.(T) -> S): Job {
+        return onEach { item -> setState { transformer(this, item) } }
+                .launchIn(this@NxMvRxViewModel + Dispatchers.Default)
+    }
+
     /**
      * Connect the given flowable to the
      * state of the viewmodel, updating
@@ -41,6 +67,12 @@ open class NxMvRxViewModel<S : MvRxState>(initialState: S) : BaseMvRxViewModel<S
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { item -> setState { transformer(this, item) } }
     }
+
+    /**
+     * Launch the collection of the given Flow
+     * on the coroutine scope of this component
+     */
+    fun Flow<*>.launchInScope(): Job = launchIn(this@NxMvRxViewModel)
 
     override fun onCleared() {
         cancelCoroutineScope()
