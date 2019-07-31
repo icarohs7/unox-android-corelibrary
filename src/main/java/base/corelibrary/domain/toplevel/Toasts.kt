5100e52a64d6dbe1d915f19b9e3d97bdac69eeba
Icaro Temponi <icarohs7@gmail.com>
2019-07-31 17:10:19 -0300

    auto commit push on 31/07/2019 17:10

--- src/main/java/base/corelibrary/domain/toplevel/Toasts.kt
+++ src/main/java/base/corelibrary/domain/toplevel/Toasts.kt
@@ -1,25 +1,16 @@
 package base.corelibrary.domain.toplevel
 
-import android.content.Context
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import splitties.resources.str
+import splitties.init.appCtx
+import splitties.resources.appStr
 
-fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
-    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(this@toast, msg, duration).show() }
+fun toast(msgRes: Int, duration: Int = Toast.LENGTH_SHORT) {
+    toast(appStr(msgRes), duration)
 }
 
-fun Context.toast(msgRes: Int, duration: Int = Toast.LENGTH_SHORT) {
-    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(this@toast, str(msgRes), duration).show() }
-}
-
-fun Fragment.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
-    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(requireContext(), msg, duration).show() }
-}
-
-fun Fragment.toast(msgRes: Int, duration: Int = Toast.LENGTH_SHORT) {
-    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(requireContext(), str(msgRes), duration).show() }
+fun toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
+    GlobalScope.launch(Dispatchers.Main) { Toast.makeText(appCtx, msg, duration).show() }
 }
