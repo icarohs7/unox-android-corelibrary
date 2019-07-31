4f124b5a0f44efe1c0012856bc245455c4577289
Icaro Temponi <icarohs7@gmail.com>
2019-07-31 16:32:11 -0300

    auto commit push on 31/07/2019 16:32

--- src/main/java/base/corelibrary/presentation/viewmodel/NxMvRxViewModel.kt
+++ src/main/java/base/corelibrary/presentation/viewmodel/NxMvRxViewModel.kt
@@ -1,11 +1,16 @@
 package base.corelibrary.presentation.viewmodel
 
+import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.BaseMvRxViewModel
+import com.airbnb.mvrx.Fail
+import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.MvRxState
 import com.airbnb.mvrx.RealMvRxStateStore
+import com.airbnb.mvrx.Success
 import com.github.icarohs7.unoxandroidarch.UnoxAndroidArch
 import com.github.icarohs7.unoxcore.extensions.coroutines.cancelCoroutineScope
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -14,8 +19,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onErrorReturn
+import kotlinx.coroutines.flow.subscribe
 import kotlinx.coroutines.plus
 
 /**
@@ -66,6 +75,23 @@ open class NxMvRxViewModel<S : MvRxState>(initialState: S) : BaseMvRxViewModel<S
         return subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { item -> setState { transformer(this, item) } }
+    }
+
+    /**
+     * Execute a flow and wrap its progression with AsyncData reduced to the global state.
+     *
+     * @param mapper A map converting the flow type to the desired AsyncData type.
+     * @param stateReducer A reducer that is applied to the current state and should return the
+     *                     new state. Because the state is the receiver and it likely a data
+     *                     class, an implementation may look like: `{ copy(response = it) }`.
+     */
+    fun <T, V> Flow<T>.execute(mapper: (T) -> V, stateReducer: S.(Async<V>) -> S): Job {
+        setState { stateReducer(Loading()) }
+
+        return map<T, Async<V>> { value -> Success(mapper(value)) }
+                .catch { emit(Fail(it)) }
+                .onEach { asyncData -> setState { stateReducer(asyncData) } }
+                .launchInScope()
     }
 
     /**
