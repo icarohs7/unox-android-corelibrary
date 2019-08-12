d4fd1079f758446fcc71a74224150255f09084c7
Icaro Temponi <icarohs7@gmail.com>
2019-08-12 09:20:46 -0300

    auto commit push on 12/08/2019 09:20

--- src/main/java/base/corelibrary/domain/extensions/coroutines/DataFlows.kt
+++ src/main/java/base/corelibrary/domain/extensions/coroutines/DataFlows.kt
@@ -2,33 +2,63 @@
 
 package base.corelibrary.domain.extensions.coroutines
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+/**
+ * An infinite flow the will broadcast sent elements
+ * to its observers
+ */
+interface SubjectFlow<T> {
+    suspend fun send(value: T)
+    fun offer(value: T): Boolean
+
+    fun observe(scope: CoroutineScope, observer: (T) -> Unit): Job
+    fun flow(): Flow<T>
+}
 
 /**
  * Wrapper class holding an [kotlinx.coroutines.channels.ArrayBroadcastChannel]
  * and a flow emitting its items
  */
-class PublishDataFlow<T> {
-    val channel: BroadcastChannel<T> = BroadcastChannel(1)
+class PublishSubjectFlow<T> : SubjectFlow<T> {
+    private val channel: BroadcastChannel<T> = BroadcastChannel(1)
 
-    fun offer(value: T): Boolean = channel.offer(value)
-    fun flow(): Flow<T> = channel.asFlow()
+    override suspend fun send(value: T): Unit = channel.send(value)
+    override fun offer(value: T): Boolean = channel.offer(value)
+
+    override fun observe(scope: CoroutineScope, observer: (T) -> Unit): Job {
+        return flow().onEach { observer(it) }.launchIn(scope)
+    }
+
+    override fun flow(): Flow<T> = channel.asFlow()
 }
 
 /**
  * Wrapper class holding an [kotlinx.coroutines.channels.ConflatedBroadcastChannel]
  * and a flow emitting its items
  */
-class BehaviorDataFlow<T>(initialValue: T? = null) {
-    val channel: ConflatedBroadcastChannel<T> = initialValue
+class BehaviorSubjectFlow<T>(initialValue: T? = null) : SubjectFlow<T> {
+    private val channel: ConflatedBroadcastChannel<T> = initialValue
             ?.let(::ConflatedBroadcastChannel)
             ?: ConflatedBroadcastChannel()
 
-    val value: T get() = channel.value
+    var value: T
+        get() = channel.value
+        set(value) = channel.offer(value).let { Unit }
 
-    fun offer(value: T): Boolean = channel.offer(value)
-    fun flow(): Flow<T> = channel.asFlow()
+    override suspend fun send(value: T) = channel.send(value)
+    override fun offer(value: T): Boolean = channel.offer(value)
+
+    override fun observe(scope: CoroutineScope, observer: (T) -> Unit): Job {
+        return flow().onEach { observer(it) }.launchIn(scope)
+    }
+
+    override fun flow(): Flow<T> = channel.asFlow()
 }
