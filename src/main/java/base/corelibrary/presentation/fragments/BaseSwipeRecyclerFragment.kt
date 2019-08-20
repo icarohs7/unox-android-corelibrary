37ffa10dadf1ed338b563d8b5cc5262aed00c087
Icaro Temponi <icarohs7@gmail.com>
2019-08-20 13:28:03 -0300

    auto commit push on 20/08/2019 13:28

--- src/main/java/base/corelibrary/presentation/fragments/BaseSwipeRecyclerFragment.kt
+++ src/main/java/base/corelibrary/presentation/fragments/BaseSwipeRecyclerFragment.kt
@@ -1,10 +1,24 @@
 package base.corelibrary.presentation.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import base.corelibrary.R
 import base.corelibrary.databinding.FragmentSwipeRecyclerBinding
 import base.corelibrary.presentation._baseclasses.BaseBindingFragment
 
 abstract class BaseSwipeRecyclerFragment : BaseBindingFragment<FragmentSwipeRecyclerBinding>() {
+    override fun onBindingCreated(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) {
+        super.onBindingCreated(inflater, container, savedInstanceState)
+        binding.stateView.hideStates()
+        binding.recycler.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    fun displayState(stateTag: String) {
+        binding.stateView.displayState(stateTag)
+    }
+
     override fun getLayout(): Int {
         return R.layout.fragment_swipe_recycler
     }
