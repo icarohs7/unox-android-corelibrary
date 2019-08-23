b173023eacbc5f4e3f33bd8963c89208319db464
Icaro Temponi <icarohs7@gmail.com>
2019-08-23 10:14:26 -0300

    auto commit push on 23/08/2019 10:14

--- src/main/java/base/corelibrary/presentation/_bindingbuilders/PartialNumberStepper.kt
+++ src/main/java/base/corelibrary/presentation/_bindingbuilders/PartialNumberStepper.kt
@@ -1,15 +1,15 @@
 package base.corelibrary.presentation._bindingbuilders
 
-import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.map
 import base.corelibrary.databinding.PartialNumberStepperBinding
 
-fun PartialNumberStepperBinding.build(lifecycle: LifecycleOwner, number: MutableLiveData<Int>) {
-    (this as ViewDataBinding).lifecycleOwner = lifecycle
-    this.number = number.map { "$it" }
-    setRemoveHandler { number.value = ((number.value ?: 0) - 1).coerceAtLeast(0) }
-    setAddHandler { number.value = (number.value ?: 0) + 1 }
+fun PartialNumberStepperBinding.build(initialValue: Int = 0, onUpdate: (Int) -> Unit) {
+    number = initialValue
+    val getNumber = { txtNumber.text.toString().toInt() }
+    setRemoveHandler {
+        if (getNumber() > 0)
+            number = getNumber() - 1; onUpdate(getNumber())
+    }
+    setAddHandler {
+        number = getNumber() + 1; onUpdate(getNumber())
+    }
 }
