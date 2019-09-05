1a9bfc161660b9c25f467f8fc0945005ec78b29b
Icaro Temponi <icarohs7@gmail.com>
2019-09-05 11:37:36 -0300

    auto commit push on 05/09/2019 11:37

--- src/main/java/base/corelibrary/domain/extensions/StringExtensions.kt
+++ src/main/java/base/corelibrary/domain/extensions/StringExtensions.kt
@@ -1,5 +1,9 @@
 package base.corelibrary.domain.extensions
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import com.github.icarohs7.unoxcore.extensions.coroutines.onBackground
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
@@ -12,4 +16,15 @@ fun <T : Any> String.deserialize(deserializer: KSerializer<T>): T {
 @ImplicitReflectionSerializer
 inline fun <reified T : Any> String.deserialize(): T {
     return Json.parse(this)
+}
+
+/**
+ * Decode the given string from base64 and convert
+ * the image into a Bitmap
+ */
+suspend fun String.base64ToBitmap(): Bitmap {
+    return onBackground {
+        val bytes = Base64.decode(substringAfter(','), Base64.DEFAULT)
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
 }
---
+++ src/main/java/base/corelibrary/domain/extensions/URLExtensions.kt
@@ -0,0 +1,40 @@
+package base.corelibrary.domain.extensions
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.util.Log
+import arrow.core.Try
+import com.chuckerteam.chucker.api.internal.support.IOUtils
+import com.github.icarohs7.unoxcore.tryBg
+import java.net.HttpURLConnection
+import java.net.URL
+
+/**
+ * Make an HTTP request to the given URL
+ * and return the body of the response
+ */
+suspend fun URL.httpGetBody(): Try<String> {
+    return tryBg {
+        openStream()
+                .bufferedReader()
+                .useLines { lines -> lines.joinToString("") }
+    }
+}
+
+/**
+ * Get the image from the given url and
+ * convert it to a bitmap
+ */
+suspend fun URL.toBitmap(): Try<Bitmap> {
+    return tryBg { openStream().use(BitmapFactory::decodeStream) }
+}
+
+/**
+ * Decode the response body of the requisition
+ * on the given URL and return the bitmap read
+ * from the base64 string
+ */
+suspend fun URL.base64ToBitmap(): Try<Bitmap> {
+    return tryBg { (!!httpGetBody()).base64ToBitmap() }
+}
