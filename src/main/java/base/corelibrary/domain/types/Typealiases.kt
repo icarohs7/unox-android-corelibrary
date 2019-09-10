6ebbbbca2de21698630db5d943f782f018090f58
Icaro Temponi <icarohs7@gmail.com>
2019-09-10 11:49:41 -0300

    auto commit push on 10/09/2019 11:49

---
+++ src/main/java/base/corelibrary/domain/types/Typealiases.kt
@@ -0,0 +1,3 @@
+package base.corelibrary.domain.types
+
+typealias Reducer<T> = T.() -> T
--- src/main/java/base/corelibrary/presentation/_baseclasses/CoreMvRxViewModel.kt
+++ src/main/java/base/corelibrary/presentation/_baseclasses/CoreMvRxViewModel.kt
@@ -9,6 +9,7 @@ import com.airbnb.mvrx.MvRxState
 import com.airbnb.mvrx.RealMvRxStateStore
 import com.airbnb.mvrx.Success
 import com.github.icarohs7.unoxandroidarch.UnoxAndroidArch
+import com.github.icarohs7.unoxcore.UnoxCore
 import com.github.icarohs7.unoxcore.extensions.coroutines.cancelCoroutineScope
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -18,13 +19,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.plus
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Base ViewModel implementing a [CoroutineScope]
@@ -35,6 +42,18 @@ open class CoreMvRxViewModel<S : MvRxState>(initialState: S) : BaseMvRxViewModel
         UnoxAndroidArch.isDebug,
         RealMvRxStateStore(initialState)
 ) {
+    /**
+     * Access the state after all pending changes
+     * are applied and invoke the given function
+     * on it, returning the result of the invocation
+     */
+    suspend fun <R> withStateSuspend(block: suspend (state: S) -> R): R {
+        return coroutineScope {
+            val state = suspendCoroutine<S> { cont -> withState { cont.resume(it) } }
+            block(state)
+        }
+    }
+
     /**
      * Connect the given [Flow] to the
      * state of the viewmodel, updating
@@ -91,6 +110,28 @@ open class CoreMvRxViewModel<S : MvRxState>(initialState: S) : BaseMvRxViewModel
                 .catch { emit(Fail(it)) }
                 .onEach { asyncData -> setState { stateReducer(asyncData) } }
                 .launchInScope()
+    }
+
+    /**
+     * Emit all changes of the state of this
+     * viewModel
+     */
+    fun stateFlow(): Flow<S> = channelFlow {
+        val disposable = subscribe { offer(it) }
+        awaitClose { disposable.dispose() }
+    }
+
+    /**
+     * Subscribe to changes on the state emitting the
+     * result of invoking the given function on each
+     * new state
+     */
+    fun <A> selectStateFlow(selectFn: suspend (S) -> A, subscriber: suspend (A) -> Unit): Flow<A> {
+        return stateFlow()
+                .map(selectFn)
+                .onEach(subscriber)
+                .distinctUntilChanged()
+                .flowOn(UnoxCore.backgroundDispatcher)
     }
 
     /**
